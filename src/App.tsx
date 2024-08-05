41cee4676b0bf13c75010a13e3e3a5e166ad29fb
Aditya Pawar <adityapawar743.ap5@gmail.com>
2024-08-05 16:13:29 +0530

    Created a basic react project with vite and bootstrap.

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+function App() {
+  return (
+    <Container>
+      <Row>
+        <Col className=""></Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default App;
